054f89d36a295d4cae63506918896780ad43f5a6
rbuchaillat <32840729+rbuchaillat@users.noreply.github.com>
2019-03-19 00:24:54 +0100

    Revert "initialize carte Leaflet"

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,20 +1,5 @@
 console.log('accueil');
 
-//Initialisation de la carte
-function initializationMap(){
-    var mymap = L.map("map").setView([48.866667, 2.333333], 12);
-
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-        maxZoom: 20,
-        attribution: '',
-        id: 'mapbox.streets'
-    }).addTo(mymap);
-
-    return mymap;
-}
-
 $(document).ready(function(){
     console.log('document is ready');
-
-    initializationMap();
 });
