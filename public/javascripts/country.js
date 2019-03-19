2b74bedb3201a766c4e5794ebe76ac531a50b623
Rémi <buchaillat.remi@hotmail.com>
2019-03-19 17:35:45 +0100

    Mise en place de l'appel à l'api

---
+++ public/javascripts/country.js
@@ -0,0 +1,9 @@
+export class Country {
+
+    constructor(name, tabArticle, tabTranslation) {
+        this.name = name;
+        this.tabArticle = tabArticle;
+        this.tabTranslation = tabTranslation;
+    }
+
+  }
