4e1398ddb11b0a55f36b8dc570ff18ffc7b043ce
Leyla LE NOAN <leyla.lenoan@outlook.fr>
2019-03-20 12:10:26 +0100

    Mise à jour du contenu de la page d'accueil : ajout de tous les éléments

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Page d\'accueil', description: 'Accueil & Affichage de la carte' });
+  res.render('ranking', { title: 'Classement', description: 'Classement des pays' });
 });
 
 module.exports = router;
