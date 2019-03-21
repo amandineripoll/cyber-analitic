4602f4a8d3a1f1a7e51b4e10a5611ef75d425118
Leyla LE NOAN <leyla.lenoan@outlook.fr>
2019-03-21 10:30:39 +0100

    Mise à jour du système de récupération sur l'api pour la map et le ranking

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,13 +1,85 @@
-import { getInformations, getCountries } from './service.js';
+import { getInformations, getCountries, getYears } from './service.js';
 import { Country } from './country.js';
 
-const tabCountries = []
-const dateStart = "20180101";
-const dateEnd = "20181231";
-const continent = "Europe";
-const country = "France";
+var tabCountries = [];
+var currentTab = [];
+var tabYears = getYears();
+var year = "2018";
+/*const year = $(document).getElementById['years'].value;
+const country = $(document).getElementById['countries'].value;*/
+
+$(document).ready(function() {
+    initializationMap();
+    getInformations().then((data) => {
+        tabCountries = data;
+        colorMap();
+    });
+});
+
+function colorMap() {
+  console.log("fin");
+  console.log(tabCountries);
+  console.log(currentTab);
+
+  var min = currentTab[0].tabArticle[0]['articles'].length;
+  var max = 0;
+  for(let i = 0; i < currentTab.length; i++) {
+    var nbArticles = 0;
+    for(let j = 0; j < currentTab[i].tabArticle.length; j++) {
+      nbArticles += currentTab[i].tabArticle[j]['articles'].length;
+    }
+    if(min > nbArticles) {
+      min = nbArticles;
+    }
+    if(max < nbArticles) {
+      max = nbArticles;
+    }
+  }
+  var med = (max - min) / 2;
+
+  /*polygonSeries.data = [{
+        "id": "US",
+        "level": 1
+      }, {
+        "id": "BR",
+        "level": 1
+      }, {
+        "id": "AU",
+        "level": 2
+      }, {
+        "id": "CN",
+        "level": 1
+      }, {
+        "id": "FR",
+        "level": 2
+      }]
+
+    // Configure series
+    var polygonTemplate = polygonSeries.mapPolygons.template;
+    polygonTemplate.tooltipText = "{name}";
+    polygonTemplate.fill = am4core.color("#004494");
+
+    // Create hover state and set alternative fill color
+    var hs = polygonTemplate.states.create("hover");
+    hs.properties.fill = am4core.color("#0682ff");
+
+    polygonTemplate.adapter.add("fill", function(fill, target) {
+        if (target.dataItem.dataContext && target.dataItem.dataContext.level == 1) {
+            return am4core.color("#FF4633");
+        }
+        return fill;
+      });
+
+    polygonTemplate.adapter.add("fill", function(fill, target) {
+        if (target.dataItem.dataContext && target.dataItem.dataContext.level == 2) {
+            return am4core.color("#FFB233");
+        }
+        return fill;
+    });*/
+}
 
 const initializationMap = () => {
+
     // Themes begin
     am4core.useTheme(am4themes_animated);
 
@@ -85,12 +157,3 @@ const initializationMap = () => {
     //     ev.target.isActive = !ev.target.isActive;
     // })
 }
-
-$(document).ready(function() {
-    initializationMap();
-    getInformations(dateStart, dateEnd, getCountries).then((data) => {
-        data.forEach((element) => {
-            tabCountries.push(new Country(element.name, element.tabArticle, element.tabTranslation));
-        });
-    });
-});
--- public/javascripts/service.js
+++ public/javascripts/service.js
@@ -1,10 +1,9 @@
 import { Country } from './country.js';
 
-const tabCountries = getCountries();
+export const getYears = () => ["2019", "2018", "2017", "2016", "2015"];
 
-export function getCountries() {
+export const getCountries = () => {
   let tab = [];
-
   tab.push(new Country("France", [], {"edition": "fr-fr","word": "harcèlement" }));
   tab.push(new Country("New York", [], {"edition": "en-us-ny","word" : "harassment" }));
   tab.push(new Country("San Francisco", [], {"edition": "en-us-df","word" : "harassment" }));
@@ -13,29 +12,33 @@ export function getCountries() {
   tab.push(new Country("Belgium NL", [], {"edition": "nl-be","word": "intimidatie" }));
   tab.push(new Country("German", [], {"edition": "de-de","word": "Belästigung" }));
   tab.push(new Country("Italia", [], {"edition": "it-it","word": "molestia" }));
-
   return tab;
 }
 
-export const getInformations = (dateStart, dateEnd) => {
+const tabCountries = getCountries();
+const tabYears = getYears();
+
+export const getInformations = () => {
     return new Promise(function(resolve, reject) {
         var cpt = 0;
-        for (let i = 0; i < tabCountries.length; i++) {
-            let request = new XMLHttpRequest();
-            request.open('GET', 'https://api.ozae.com/gnw/articles?date='+dateStart+'__'+dateEnd+'&edition='+tabCountries[i].tabTranslation['edition']+'&query='+tabCountries[i].tabTranslation['word']+'&key=646c7b6710c14533be68450f2d61d15d', true)
-            request.onload = function() {
-                let data = JSON.parse(this.response);
-                if (request.status >= 200 && request.status < 400) {
-                    tabCountries[i].tabArticle = data;
-                    cpt++;
-                    if(cpt == tabCountries.length) {
-                        resolve(tabCountries);
+        for (let i = 0; i < tabYears.length; i++) {
+            for (let j = 0; j < tabCountries.length; j++) {
+                let request = new XMLHttpRequest();
+                request.open('GET', 'https://api.ozae.com/gnw/articles?date='+tabYears[i]+'0101__'+tabYears[i]+'1231&edition='+tabCountries[j].tabTranslation['edition']+'&query='+tabCountries[j].tabTranslation['word']+'&key=646c7b6710c14533be68450f2d61d15d', true)
+                request.onload = function() {
+                    let data = JSON.parse(this.response);
+                    if (request.status >= 200 && request.status < 400) {
+                        tabCountries[j].tabArticle.push({"year": tabYears[i], "articles": data['articles']});
+                        cpt++;
+                        if(cpt == tabCountries.length * tabYears.length) {
+                            resolve(tabCountries);
+                        }
+                    } else {
+                        reject('error');
                     }
-                } else {
-                    reject('error');
                 }
+                request.send();
             }
-            request.send();
         }
     });
 }
